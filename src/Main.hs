bd660e3e9311a74813aeb8371fb7e237ae051c7c
Niko Salakka <niko@IMAC-9245q>
2014-09-10 14:35:15 +0300

    example of displaying the data

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,16 @@
 module Main (main) where
 
 import Heather
+import Data.Text
+import Data.Maybe
 
 main :: IO ()
 main = do
     w <- getWeather "helsinki"
     print w
+    case w of
+        Just (Weather {}) -> print $ tellW $ fromJust w
+        Nothing -> print "nope"
+
+tellW :: Weather -> String
+tellW w = "Wheather is made of " ++ unpack (weather w) ++ ", " ++ unpack (description w) ++ " to be exact. Temperature is around " ++ show ((temp w) - 273.15) ++ " MURICAs and there's a " ++ show (wSpeed w) ++ " m/s wind from the " ++ show (wDeg w)
