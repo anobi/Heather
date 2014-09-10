0e5147ea428541e362b2364e760a149dc65ff19c
Niko Salakka <niko@IMAC-9245q>
2014-09-10 13:17:29 +0300

    fully working - now parsing rest of the stuff

--- src/Heather.hs
+++ src/Heather.hs
@@ -1,16 +1,16 @@
---{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Heather where
 
 import Data.Text (Text)
-import Data.Aeson as AS
+import Data.Aeson 
 import Control.Applicative
 import Control.Monad
 import Network.HTTP.Conduit
 
 data Weather = Weather { 
-    --weather         :: !Text,
-    --description     :: !Text,
+    weather         :: !Text,
+    description     :: !Text,
     temp            :: Double,
     pressure        :: Double,
     humidity        :: Double,
@@ -22,14 +22,14 @@ data Weather = Weather {
 
 instance FromJSON Weather where
     parseJSON (Object v) = Weather <$> 
-    --    ((v .: "weather") >>= (.: "speed"))         <*>
-     --   ((v .: "weather") >>= (.: "description"))   <*>
-        ((v .: "main") >>= (.: "temp"))             <*>
-        ((v .: "main") >>= (.: "pressure"))         <*>
-        ((v .: "main") >>= (.: "humidity"))         <*>
-        ((v .: "main") >>= (.: "temp_min"))         <*>
-        ((v .: "main") >>= (.: "temp_max"))         <*>
-        ((v .: "wind") >>= (.: "speed"))            <*>
+        ((head <$> v .: "weather") >>= (.: "main"))         <*>
+        ((head <$> v .: "weather") >>= (.: "description"))  <*>
+        ((v .: "main") >>= (.: "temp"))                     <*>
+        ((v .: "main") >>= (.: "pressure"))                 <*>
+        ((v .: "main") >>= (.: "humidity"))                 <*>
+        ((v .: "main") >>= (.: "temp_min"))                 <*>
+        ((v .: "main") >>= (.: "temp_max"))                 <*>
+        ((v .: "wind") >>= (.: "speed"))                    <*>
         ((v .: "wind") >>= (.: "deg"))
     parseJSON _ = mzero
 
