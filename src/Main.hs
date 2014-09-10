c68454f54fe8349e29813689d01c01aabaccc6d5
Niko Salakka <niko@IMAC-9245q>
2014-09-10 14:37:23 +0300

    asd

--- src/Main.hs
+++ src/Main.hs
@@ -7,10 +7,9 @@ import Data.Maybe
 main :: IO ()
 main = do
     w <- getWeather "helsinki"
-    print w
     case w of
         Just (Weather {}) -> print $ tellW $ fromJust w
         Nothing -> print "nope"
 
 tellW :: Weather -> String
-tellW w = "Wheather is made of " ++ unpack (weather w) ++ ", " ++ unpack (description w) ++ " to be exact. Temperature is around " ++ show ((temp w) - 273.15) ++ " MURICAs and there's a " ++ show (wSpeed w) ++ " m/s wind from the " ++ show (wDeg w)
+tellW w = "Wheather is made of " ++ unpack (weather w) ++ ", " ++ unpack (description w) ++ " to be exact. Temperature is around " ++ show ((temp w) - 273.15) ++ "C and there's a " ++ show (wSpeed w) ++ " m/s wind from the " ++ show (wDeg w)
